3aa5b2386beda9d09c86f8e6eddb5ab77a3f7ba2
Adam JOLY <adjoly@student.42angouleme.fr>
2025-01-24 15:58:03 +0100

    「🏗️」 wip: work in progress, not done yet.

--- includes/core/toml.hpp
+++ includes/core/toml.hpp
@@ -1,13 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tomlpp.cpp                                         :+:      :+:    :+:   */
+/*   toml.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/24 14:35:34 by adjoly            #+#    #+#             */
-/*   Updated: 2025/01/24 14:37:26 by adjoly           ###   ########.fr       */
+/*   Created: 2025/01/24 14:38:20 by adjoly            #+#    #+#             */
+/*   Updated: 2025/01/24 15:57:59 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include <cstdlib>
+#include <string>
+
+typedef	std::string tomlKey;
+
+typedef	struct {
+	tomlKey	key;
+	void	*val;
+}	tomlPair_t;
+
+class toml {
+public:
+	toml();
+
+	~toml();
+	tomlPair_t	getPair(std::string keyName);
+	tomlKey		getKey(std::string keyName);
+private:
+	tomlPair_t* val;
+};
--- includes/tomlpp.h
+++ includes/tomlpp.h
@@ -6,17 +6,21 @@
 /*   By: adjoly <adjoly@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/24 13:53:22 by adjoly            #+#    #+#             */
-/*   Updated: 2025/01/24 13:53:23 by adjoly           ###   ########.fr       */
+/*   Updated: 2025/01/24 15:43:06 by adjoly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
+#include "core/parser.hpp"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef	toml toml;
 
+toml	toml_get_file();
 
 #ifdef __cplusplus
 }
